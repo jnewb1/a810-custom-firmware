b8bd4dcbb79a1cf53d6fdbb7fc9f2d260fe1b308
Justin Newberry <jnewberry0502@gmail.com>
2024-11-11 23:50:18 +0000

    fix extract

--- src/extract.sh
+++ src/extract.sh
@@ -9,28 +9,26 @@ function bin_info() {
 
 function extract_from_bin() {
     partition=$1
-    $RUN python /opt/ntfwinfo/NTKFWinfo.py -i $FIRMWARE -u $partition -o ../extracted
+    $RUN /bin/bash -c "pushd original && \
+         python /opt/ntfwinfo/NTKFWinfo.py -i $FIRMWARE -u $partition -o ../extracted && \
+         popd original"
 }
 
 
 function extract_cpio() {
     # Extract cpio rootfs partition
     partition=$1
-    $RUN mkdir ../extracted/$FIRMWARE-uncomp_partitionID${partition}_cpio
-    $RUN cpio -idmv --file ../extracted/$FIRMWARE-uncomp_partitionID${partition} --directory ../extracted/$FIRMWARE-uncomp_partitionID${partition}_cpio
+    $RUN mkdir extracted/$FIRMWARE-uncomp_partitionID${partition}_cpio
+    $RUN cpio -idmv --file extracted/$FIRMWARE-uncomp_partitionID${partition} --directory extracted/$FIRMWARE-uncomp_partitionID${partition}_cpio
 }
 
 
 function extract_all() {
-    pushd original
-
     for partition in 5 8 9 11 13; do
         extract_from_bin $partition;
     done
 
     extract_cpio 8
-
-    popd
 }
 
 #bin_info
